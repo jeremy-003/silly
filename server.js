bc2ab80000f8f7b5a473705e541b5b8c6c73a7ed
Jeremy Rollins <jeremy.rollins@endurance.com>
2019-12-13 14:35:04 -0700

    removed the options in the res.render in the get / to see if it will still work

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ function isValidPartialMySite(inMySite) {
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  res.render('index.html', { pageCountMessage : null});
+  res.render();
 });
 
 // error handling
