ae064b5a1b717a910c633c5c3e9bd6744bd48202
Jeremy Rollins <jeremy.rollins@endurance.com>
2019-12-19 09:54:10 -0700

    re-added the code I removed to test for which conditional it uses since I was getting odd errors

--- server.js
+++ server.js
@@ -10,19 +10,32 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     mongoURLLabel = "";
 
 if (mongoURL == null) {
+  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+  // If using plane old env vars via service discovery
+  if (process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
+    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
+    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
+    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
+    mongoUser = process.env[mongoServiceName + '_USER'];
+    console.log('Using plain old env vars via service discovery')
 
-  mongoDatabase = process.env.database_name;
-  mongoPassword = process.env.password;
-  mongoUser = process.env.username;
-  var mongoUriParts = process.env.uri && process.env.uri.split("//");
-  if (mongoUriParts.length == 2) {
-    mongoUriParts = mongoUriParts[1].split(":");
-    if (mongoUriParts && mongoUriParts.length == 2) {
-      mongoHost = mongoUriParts[0];
-      mongoPort = mongoUriParts[1];
+  // If using env vars from secret from service binding  
+  } else if (process.env.database_name) {
+    console.log('using env vars from secrets')
+    mongoDatabase = process.env.database_name;
+    mongoPassword = process.env.password;
+    mongoUser = process.env.username;
+    var mongoUriParts = process.env.uri && process.env.uri.split("//");
+    if (mongoUriParts.length == 2) {
+      mongoUriParts = mongoUriParts[1].split(":");
+      if (mongoUriParts && mongoUriParts.length == 2) {
+        mongoHost = mongoUriParts[0];
+        mongoPort = mongoUriParts[1];
+      }
     }
   }
-  
 
   if (mongoHost && mongoPort && mongoDatabase) {
     mongoURLLabel = mongoURL = 'mongodb://';
