97b87f998facf4307ae800eaafebd219e2ab7d39
Jeremy Rollins <jeremy.rollins@endurance.com>
2019-12-19 09:35:22 -0700

    removed some unused code about env variables that were never used

--- server.js
+++ server.js
@@ -11,27 +11,18 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 if (mongoURL == null) {
   var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
-  // If using plane old env vars via service discovery
-  if (process.env.DATABASE_SERVICE_NAME) {
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
-    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
-    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
-    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
-    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
-    mongoUser = process.env[mongoServiceName + '_USER'];
+  
+  mongoDatabase = process.env.database_name;
+  mongoPassword = process.env.password;
+  mongoUser = process.env.username;
 
-  // If using env vars from secret from service binding  
-  } else if (process.env.database_name) {
-    mongoDatabase = process.env.database_name;
-    mongoPassword = process.env.password;
-    mongoUser = process.env.username;
-    var mongoUriParts = process.env.uri && process.env.uri.split("//");
-    if (mongoUriParts.length == 2) {
-      mongoUriParts = mongoUriParts[1].split(":");
-      if (mongoUriParts && mongoUriParts.length == 2) {
-        mongoHost = mongoUriParts[0];
-        mongoPort = mongoUriParts[1];
-      }
+  var mongoUriParts = process.env.uri && process.env.uri.split("//");
+  
+  if (mongoUriParts.length == 2) {
+    mongoUriParts = mongoUriParts[1].split(":");
+    if (mongoUriParts && mongoUriParts.length == 2) {
+      mongoHost = mongoUriParts[0];
+      mongoPort = mongoUriParts[1];
     }
   }
 
@@ -44,6 +35,12 @@ if (mongoURL == null) {
     mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
     mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
   }
+}
+if (mongoURLLabel == "") {
+  ip = '127.0.0.1';
+  port = 3000;
+  mongoURLLabel = mongoURL = 'mongodb://localhost:27017/sampledb';
+  mongoDatabase = 'sherlock';
 }
 var db = null,
     dbDetails = new Object();
